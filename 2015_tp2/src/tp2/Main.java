a6ae2e14f814c93164579c7c4ee02daf66764f93
1-jeongeun <alysia.je.lee@gmail.com>
2022-11-27 22:24:27 -0500

    Update Main.java

--- 2015_tp2/src/tp2/Main.java
+++ 2015_tp2/src/tp2/Main.java
@@ -1,5 +1,13 @@
 package tp2;
 
-public class Main {
+// import java.io.*;
 
+public class Main {
+    /**
+     * Driver class method.
+     * @param args Not used.
+     * @throws Exception Throws file exception.
+     */
+    public static void main(String[] args) throws Exception {
+    }
 }
