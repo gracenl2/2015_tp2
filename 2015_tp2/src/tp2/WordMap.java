0098900dc92107bb4a9848b63ac20835abe5a06d
1-jeongeun <alysia.je.lee@gmail.com>
2022-12-07 00:30:40 -0500

    Update WordMap.java

--- 2015_tp2/src/tp2/WordMap.java
+++ 2015_tp2/src/tp2/WordMap.java
@@ -1,67 +1,178 @@
 package tp2;
 
-import java.util.Collection;
-// import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+// import java.util.Collection;
+// // import java.util.Iterator;
+// import java.util.Map;
+// import java.util.Set;
 
-public class WordMap<K,V> implements Map<K,V> {
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.requireNonNull;
+
+
+public class WordMap<K, V> implements Map<K, V> {
+
+    private ArrayList<WordEntry<K, V>>[] table;
+
+    private final int defaultSize = 10;
+    private int size = 0;
+
+    private static class WordEntry<K, V> implements Map.Entry<K, V> {
+        K key;
+        V value;
+
+        public WordEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return this.key;
+        }
+
+        @Override
+        public V getValue() {
+            return this.value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V temp = this.value;
+            this.value = value;
+            return temp;
+        }
+    }
+
+    private int indexFor(Object key) {
+        return key.hashCode() % table.length;
+    }
+
+    public WordMap() {
+        table = new ArrayList[defaultSize];
+        for (int i = 0; i < defaultSize; i++) {
+            table[i] = new ArrayList<>();
+        }
+    }
+
+
     @Override
     public int size() {
-        return 0; 
-
+        return size; 
     }
+
     @Override
     public boolean isEmpty() {
-        return false; 
-
+        return size == 0; 
     }
+
     @Override
     public boolean containsKey(Object key) {
-        return false; 
+        requireNonNull(key);
 
+        int index = indexFor(key);
+
+        return table[index].stream().anyMatch((e) -> e.getKey().equals(key)); 
     }
+
     @Override
     public boolean containsValue(Object value) {
-        return false; 
+        requireNonNull(value);
 
+        return Arrays.stream(table).anyMatch((l) -> l.stream().anyMatch((e) -> e.getValue().equals(value)));
     }
+
     @Override
     public V get(Object key) {
-        return null;
+        requireNonNull(key);
 
+        int index = indexFor(key);
+
+        for (WordEntry<K, V> entry : table[index]) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+        return null;
     }
+
     @Override
     public V put(K key, V value) {
-        return value;
+        requireNonNull(key);
+        requireNonNull(value);
 
+        int index = indexFor(key);
+
+        for (WordEntry<K, V> entry : table[index]) {
+            if (entry.getKey().equals(key)) {
+                V oldValue = entry.getValue();
+                entry.setValue(value);
+                return oldValue;
+            }
+        }
+        table[index].add(new WordEntry<K, V>(key, value));
+        size++;
+        return value;
     }
+
     @Override
     public V remove(Object key) {
-        return null;
+        requireNonNull(key);
 
+        int index = indexFor(key);
+
+        for (WordEntry<K, V> entry : table[index]) {
+            if (entry.getKey().equals(key)) {
+                V oldValue = entry.getValue();
+                table[index].remove(entry);
+                return oldValue;
+            }
+        }
+        return null;
     }
+
     @Override
     public void putAll(Map<? extends K,? extends V> m) {
+        requireNonNull(m);
 
+        if (!m.isEmpty()) {
+            m.keySet().stream()
+                    .forEach((k) -> put(k, m.get(k)));
+        }
     }
+
     @Override
     public void clear() {
-
+        Arrays.stream(table)
+                .forEach((l) -> l.clear());
+        size = 0;
     }
+
     @Override
     public Set<K> keySet() {
-        return null;
-
+        Set<K> set = Arrays.stream(table)
+                .flatMap((l) -> l.stream()
+                        .map((e) -> e.getKey()))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        return set;
     }
+
     @Override
     public Collection<V> values() {
-        return null;
-
+        Collection<V> collection = Arrays.stream(table)
+                .flatMap((l) -> l.stream()
+                        .map((e) -> e.getValue()))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return collection;
     }
+
     @Override
     public Set<Map.Entry<K,V>> entrySet() {
-        return null;
-        
+        Set<Map.Entry<K, V>> set = Arrays.stream(table)
+                .flatMap((l) -> l.stream()
+                        .map((e) -> new AbstractMap.SimpleEntry<K, V>(e.getKey(), e.getValue())))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        return set;
     }
 }
