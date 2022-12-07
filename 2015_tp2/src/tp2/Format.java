887368e967be59598bb85e32996c349abcfff9d1
1-jeongeun <alysia.je.lee@gmail.com>
2022-12-06 23:09:14 -0500

    Update Format.java

--- 2015_tp2/src/tp2/Format.java
+++ 2015_tp2/src/tp2/Format.java
@@ -1,35 +1,35 @@
-package tp2;
+// package tp2;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+// import java.io.BufferedReader;
+// import java.io.File;
+// import java.io.FileNotFoundException;
+// import java.io.FileReader;
+// import java.io.IOException;
 
-public class Format {
+// public class Format {
 	
-	public static void main(String[] args) throws IOException {
-		try {
+// 	public static void main(String[] args) throws IOException {
+// 		try {
 			
-			File myObj = new File("/Users/jeongeun.lee/Documents/2015_tp2/2015_tp2/src/tp2/dataset/900.txt");
-			BufferedReader myReader = new BufferedReader(new FileReader (myObj));
+// 			File myObj = new File("/Users/jeongeun.lee/Documents/2015_tp2/2015_tp2/src/tp2/dataset/900.txt");
+// 			BufferedReader myReader = new BufferedReader(new FileReader (myObj));
 			
-			String text;
+// 			String text;
 			
-			while ((text = myReader.readLine()) != null) {
-				text = text.replaceAll("[^\\w\\s\\ ]", " ");
-				text = text.toLowerCase();
-				System.out.println(text);
-			}
+// 			while ((text = myReader.readLine()) != null) {
+// 				text = text.replaceAll("[^\\w\\s\\ ]", " ");
+// 				text = text.toLowerCase();
+// 				System.out.println(text);
+// 			}
 			
-			myReader.close();
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("No file found.");
-			e.printStackTrace();
-	}
+// 			myReader.close();
+// 		}
+// 		catch (FileNotFoundException e) {
+// 			System.out.println("No file found.");
+// 			e.printStackTrace();
+// 	}
 	
 	 
-}	    
+// }	    
 
-}
+// }
