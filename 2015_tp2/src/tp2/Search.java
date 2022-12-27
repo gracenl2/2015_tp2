a767e945d2a361dd8718749f3bc9444d5766c4df
gracelee <gracelee@localhost>
2022-12-26 23:31:08 -0500

    final search

--- 2015_tp2/src/tp2/Search.java
+++ 2015_tp2/src/tp2/Search.java
@@ -1,15 +1,14 @@
 package tp2;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
-import com.sun.jdi.Value;
-import com.sun.tools.javac.util.Context.Key;
+
 
 public class Search {
 	File[] filelist;
-	Map<Key,Value> WordMap;
+	Map<String,Integer> WordMap;
+	
 	
 	public void SearchInFile(String word[])
 	{
@@ -20,7 +19,7 @@ public class Search {
 		public void setWords(String word[]){
 		    for(int i=1;i<word.length;i++)
 		    {
-		        Map<Key, Value> WordMap;
+		        WordMap = null;
 		        WordMap.put(word[i],0);
 		    }
 		
